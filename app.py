c3d86ff6b10af7c73896dff2283fb8d5e6430d98
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-14 14:57:58 +0900

    DB 버전으로 교체

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import psycopg2
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from datetime import datetime
 
 app = Flask(__name__)
@@ -9,14 +9,13 @@ app.secret_key = "super_secret_key"
 DATABASE_URL = os.environ.get("DATABASE_URL")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD")
 
-# DB 연결 함수
-def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL)
-    return conn
+# DB 연결
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
 
 # 테이블 자동 생성
 def create_table():
-    conn = get_db_connection()
+    conn = get_connection()
     cur = conn.cursor()
     cur.execute("""
         CREATE TABLE IF NOT EXISTS inquiries (
@@ -25,7 +24,7 @@ def create_table():
             phone TEXT,
             message TEXT,
             created_at TIMESTAMP
-        );
+        )
     """)
     conn.commit()
     cur.close()
@@ -33,9 +32,11 @@ def create_table():
 
 create_table()
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route("/contact", methods=["POST"])
 def contact():
@@ -44,10 +45,10 @@ def contact():
     message = request.form["message"]
     now = datetime.now()
 
-    conn = get_db_connection()
+    conn = get_connection()
     cur = conn.cursor()
     cur.execute(
-        "INSERT INTO inquiries (name, phone, message, created_at) VALUES (%s, %s, %s, %s)",
+        "INSERT INTO inquiries (name, phone, message, created_at) VALUES (%s,%s,%s,%s)",
         (name, phone, message, now)
     )
     conn.commit()
@@ -55,3 +56,34 @@ def contact():
     conn.close()
 
     return "<h2>문의 접수 완료!</h2><a href='/'>돌아가기</a>"
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        if request.form["password"] == ADMIN_PASSWORD:
+            session["admin"] = True
+            return redirect("/admin")
+        else:
+            return "비밀번호 틀림"
+
+    return render_template("login.html")
+
+
+@app.route("/admin")
+def admin():
+    if not session.get("admin"):
+        return redirect("/login")
+
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM inquiries ORDER BY created_at DESC")
+    data = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return render_template("admin.html", inquiries=data)
+
+
+if __name__ == "__main__":
+    app.run()
