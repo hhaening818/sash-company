fdfb72529ff892020671c2e4a9cdff2a3696c138
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-17 08:59:31 +0900

    일단 수정

--- app.py
+++ app.py
@@ -18,13 +18,13 @@ import time
 import base64
 from flask import jsonify
 from datetime import timedelta
+from datetime import datetime, timedelta, timezone
 
 cloudinary.config(
     cloud_name=os.getenv("CLOUD_NAME"),
     api_key=os.getenv("API_KEY"),
     api_secret=os.getenv("API_SECRET")
 )
-
 
 app = Flask(__name__)
 app.secret_key = "super_secret_key"
