612ace801f6d027b3580f3be6a24cd2e1dcb68d7
hhaening818 <hhaening818@users.noreply.github.com>
2026-02-19 14:32:00 +0900

    왜 안되는거임?..

--- app.py
+++ app.py
@@ -285,6 +285,16 @@ def contact():
 
     return redirect("/")
 
+
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+
+    print("Starting Flask on port", port)
+
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
+
